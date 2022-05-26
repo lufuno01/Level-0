b0c5c523a460a6ee0a4dfaaec112b41467f18da4
lufuno01 <104906398+lufuno01@users.noreply.github.com>
2022-05-26 13:43:27 +0200

    Task 0.10

---
+++ task0.10.py
@@ -0,0 +1,8 @@
+def common_characters(first_word, second_word):
+
+    for letter in first_word.lower() and second_word.lower():
+        if letter in first_word and second_word:
+            print(letter, end=",")
+
+
+common_characters("House", "Computers")

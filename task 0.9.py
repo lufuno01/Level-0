7bb13fa9d8525bbf091f93ed05b39707dad46e1a
lufuno01 <104906398+lufuno01@users.noreply.github.com>
2022-06-02 22:06:57 +0200

    task 0.9

--- task 0.9.py
+++ task 0.9.py
@@ -1,7 +1,20 @@
+
 def checking_vowels(word):
-    for vowel in word.lower():
-        if vowel in "aeiou":
-            print(vowel)
+    vowels = ["a", "e", "i", "o", "u"]
+    no_duplicates_found = []
+    vowels_found = []
+    word.lower()
+    for x in word:
+        if x in vowels:
+            vowels_found.append(x)
+
+    for i in vowels_found:
+        if i not in no_duplicates_found:
+            no_duplicates_found.append(i)
+    results = ", ".join(no_duplicates_found)
+    print(results)
 
 
 checking_vowels("Umuzi")
+   
+  
